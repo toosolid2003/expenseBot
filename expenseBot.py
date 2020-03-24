6574e2b8671427c285c2f3359c34fbd7a0f1fd15
Thibaut <thibaut.segura@yahoo.fr>
2020-03-24 12:33:16 +0100

    First stab at using the telegram.ext library. Not successful.

--- expenseBot.py
+++ expenseBot.py
@@ -1,117 +1,49 @@
 # Source for this script: https://www.codementor.io/@garethdwyer/building-a-chatbot-using-telegram-and-python-part-2-sqlite-databse-backend-m7o96jger?icn=post-goi5fncay&ici=post-m7o96jger
-
-import json
-import requests
 import time
-import urllib
 from dbhelper import DBHelper
-from io import BytesIO
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
 
+#Initiates DB and creates an updater object used in the whole program
 db = DBHelper()
 TOKEN = '994986692:AAF2wlYCT9_KIbLVxCRLNVVNfQMM9NJJJmA'
-URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-# Basic functions
-def get_url(url):
+#Set up a logging function to know when things go south.
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
-    return content
 
-def getJsonFromUrl(url):
-    '''Gets a string response from Requests and parse it into a dictionnary'''
-    content = get_url(url)
-    js = json.loads(content)
-    return js
 
-# Functions to interact with the API
-def sendMsg(text, chatId):
-    '''Sends a message to the bot.'''
 
-    text = urllib.parse.quote_plus(text)
-    url = URL + 'sendMessage?text={}&chat_id={}'.format(text, chatId)
-    get_url(url)
+#Make space for the functions
 
-def getUpdates(offset=None):
-    '''Get the latest updates from Telegram'''
-    url = URL + 'getUpdates?timeout=100'
-    if offset:
-        url += '&offset={}'.format(offset)
-    js = getJsonFromUrl(url)
-    return js
+def start(update, context):
+    '''Function to reply to the /start command.'''
 
-def getfileId(updates, lastUpdateId):
-    '''Extracts the fie Id of the high-definition pciture we want to save.
-    Returns a string, the fileId'''
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome! I'm your expense bot, throw me your expenses and I'll make sure they're logged in IQ Navigator")
 
-    fileId = updates['result'][lastUpdateId]['message']['photo'][2]['file_id']
-    return fileId
-
-def downloadPic(file_id):
-    '''Download a picture from the Telegram API and returns a BLOB ready for the database'''
-    #Get the file path fron Telegram API
-    url = URL + 'getFile?file_id={}'.format(file_id)
-    answer = getJsonFromUrl(url)
-    filePath = answer['result']['file_path']
-
-    #Download the file from Telegram API, different URL
-    urlDownload = 'https://api.telegram.org/file/bot' + TOKEN + '/' + filePath
-    photo = requests.get(urlDownload)
-
-    return photo.content
-
-def hasPicture(updates, lastUpdateId):
-    '''Checks if a new message has a picutre in it. In which case, it sends back True'''
-
-    try:
-        updates['result'][lastUpdateId]['message']['photo']
-        return True
-    except KeyError:
-        return False
+def hasPic(update, context):
+    '''Stores pic it in data['receipt']'''
+    photo_file = update.message.photo[-1].get_file()
+    breakpoint()
+    photo_file.download('receipt.jpg')
+    update.message.reply_text('Nice one. Thanks for the receipt')
 
 def main():
     # Initiate all variables 
     db.setup()
-    lastUpdateId = 0
-    offset = None
     data = {'date':time.strftime("%Y-%m-%d"), 'reason':None,'status':'pending','blob':None,'amount':None,'wbs':'BLFG101X'}
 
-    #Loop through last update to determine what's the best action
-    while True:
-        updates = getUpdates(offset)
-        if len(updates['result']) > 0:
-                lastUpdateId = len(updates['result']) - 1
-                chat_id = updates['result'][lastUpdateId]['message']['chat']['id']
+    #Picture handler
+    pic_handler = MessageHandler(Filters.document.category("image"), hasPic)
+    dispatcher.add_handler(pic_handler)
 
-                if hasPicture(updates, lastUpdateId):
-                    fileId = getfileId(updates, lastUpdateId)
-                    data['blob'] = downloadPic(fileId)
-                else:
-                    msgContent = updates['result'][lastUpdateId]['message']['text']
-                    #try to convert it as float - in case it's a number
-                    try:
-                        data['amount'] = float(msgContent)
-                        sendMsg('Got your amount', chat_id)
-                    except:
-                        data['reason'] = msgContent
-                        sendMsg('Got the reason', chat_id)
-
-                #Need to get the last update_id to pass it as offset number on the next call for updates
-                offset = updates['result'][lastUpdateId]['update_id'] + 1
-
-                #Create the data tuple and inject it in the DB
-                if data['amount'] and data['reason'] and data['blob']:
-                    data_tuple = (data['amount'], data['date'], data['reason'],data['status'], data['wbs'], data['blob'])
-                    try:
-                        db.add_item(data_tuple)
-                        sendMsg('All good bro, expense recorded.', chat_id)
-                        #Reinitialise the data dictionnary
-                        data = {'date':time.strftime("%Y-%m-%d"), 'reason':None,'status':'pending','blob':None,'amount':None,'wbs':'BLFG101X'}
-                    except:
-                        sendMsg('I ran into an issue logging the expense, sorry', chat_id)
-                else:
-                    continue
-        time.sleep(0.5)
+    #Start long polling
+    updater.start_polling()
 
 if __name__=='__main__':
     main()
