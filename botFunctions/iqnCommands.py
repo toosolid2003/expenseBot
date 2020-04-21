1f059c24675186c398ab058f70db64ec628f7a39
Thibaut <thibaut.segura@yahoo.fr>
2020-04-21 15:53:15 +0200

    adding more functions to iqnFunctions

--- botFunctions/iqnCommands.py
+++ botFunctions/iqnCommands.py
@@ -71,20 +71,121 @@ def createExpenseReport(driver):
     return driver
 
 
-def addExpense(driver, exp):
+def addExpense(driver, expObkList):
     """
     Records a new expense in the Add Expense form.
-    Input: browser (driver) with Add Expense form open, expense objet
+    Input: browser (driver) with Add Expense form open, list of expense objets
     Output: browser (driver) after all expenses are recorded.
 
     """
+    #Creating a dictionnary to host the names of the fields on the Add Expense form
+    fields = {'date':'entryFieldsContainer:fieldGroup:fields:1:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:textBox',
+            'amount':'entryFieldsContainer:fieldGroup:fields:3:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:textField',
+            'reason':'entryFieldsContainer:fieldGroup:fields:4:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:textField',
+            'receipt':'entryFieldsContainer:fieldGroup:fields:5:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:container:attachmentPanel:feedback:border:feedback_body:fileInput',
+            'attachBtn':'entryFieldsContainer:fieldGroup:fields:5:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:container:attachmentPanel:feedback:border:feedback_body:fileSubmit:container:container_body:button',
+            'wbs':'entryFieldsContainer:fieldGroup:fields:7:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:sizingWrapper:textField:autocompleteField',
+            'type':'entryFieldsContainer:fieldGroup:fields:2:feedbackReportingBorder:border:feedbackReportingBorder_body:fieldWrapper:field:select'}
+
+    # Add Expense form - start of the loop
+    j = 0
+    ## Enter date
+    for exp in expObjList:
+
+        print('Adding expense {}'.format(j))
+
+        #Enter type (html select)
+        print('Adding type: {}'.format(exp.type))
+        select_element = driver.find_element_by_name(fields['type'])
+        select_object = Select(select_element)
+        select_object.select_by_visible_text(exp.type)
+        time.sleep(2)
+
+        #Enter date of expense
+        print('Adding date: {}'.format(exp.date))
+        label = driver.find_element_by_name(fields['date'])
+        label.send_keys(exp.date)
+        #label.send_keys(Keys.TAB)
+        #time.sleep(2)
+
+        #Enter Amount
+        print('Adding amount: {}'.format(exp.amount))
+        label = driver.find_element_by_name(fields['amount'])
+        label.send_keys(str(exp.amount))
+        #label.send_keys(Keys.TAB)
+        #time.sleep(2)
+
+        #Enter reason
+        print('Adding reason: {}'.format(exp.reason))
+        label = driver.find_element_by_name(fields['reason'])
+        label.send_keys(exp.reason)
+        #label.send_keys(Keys.TAB)
+        #time.sleep(2)
+
+        #Upload receipt
+        print('Uploading receipt: {}'.format(exp.receipt))
+        btn = driver.find_element_by_name(fields['receipt'])
+        btn.send_keys(exp.receipt)
+        time.sleep(2)
+
+        #Click on 'Attach' button
+        buttn = driver.find_element_by_name(fields['attachBtn'])
+        buttn.click()
+        time.sleep(4)  #Wait for receipt to load
+
+        #Enter WBS
+        print('Adding WBS: {}'.format(exp.wbs))
+        select_element = driver.find_element_by_name(fields['wbs'])
+        select_element.send_keys(exp.wbs)
+        select_element.send_keys(Keys.TAB)
+
+        #Save and Add other expense
+        print('Saving expense')
+        #Use TAB key to scroll down to have the Save and Add Button appear to the driver
+        time.sleep(2)
+        btn = driver.find_element_by_name('saveAndAddButton:container:container_body:button')
+        btn.click()
+        time.sleep(5)
+#        #Take a screenshot here
+#        driver.save_screenshot('diagnostic.png')
+#        try:
+#            feedback = driver.find_element_by_class_name('fbERROR')
+#            print('Error on the form')
+#        except NoSuchElementException:
+#            feedback = driver.find_element_by_class_name('fbINFO')
+#            print('Expense recorded, off to the next')
+        #Save and Close
+        #saveNclose = driver.find_element_by_name('saveAndCloseButton:container:container_body:button')
+        #saveNclose.click()
+        time.sleep(2)
+        j += 1
+
 
 def saveExpenseReport(driver):
     """
     Saves the draft of an expense report.
-    Input: driver positionned on the Expense Report page.
-    Output: driver after expense report is saved
+    Input: driver positionned on the Add Expense form
+    Output: driver closed.
     """
+
+    #Close the Add Expense form
+    closeBtn = driver.find_element_by_class_name('container-close')
+    closeBtn.click()
+
+    time.sleep(2)
+    #Save the draft
+    print('Saving the draft of expense report')
+    elts = driver.find_elements_by_class_name('actionButtonLabel')
+    elts[1].click()
+    time.sleep(4)
+
+    #Logout
+    print('Logging out')
+    logout = driver.find_element_by_id('logoutLink')
+    logout.click()
+
+    #Close Chrome
+    driver.close()
 
 def submitExpenseReport(driver):
     """
