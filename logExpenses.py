d320d479543f08e85d388aba20bac16066be4a14
Thibaut Segura <toosolid2003@yahoo.fr>
2020-05-04 08:26:42 +0000

    Removed 3 unnecessary files

--- logExpenses.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-from botFunctions.iqnCommands import *
-driver = login('tsegura2','Brutasse1-')
-driver = createExpenseReport(driver)
-pd = createExpensesList('thedropper')
-driver = addExpense(driver, pd)
-driver = saveExpenseReport(driver)
