3831d1e0197a7a1b9646e2bdccc17d85007ed6dd
Thib <toosolid2003@yahoo.fr>
2022-01-23 11:39:11 +0000

    botParams with proper ssl certificate paths

--- botParams.py
+++ botParams.py
@@ -1,7 +1,4 @@
-from telegram import Bot
+KEY ='/var/www/expenseBot/ssl/PRODPRIVATE.key'
+CERT ='/var/www/expenseBot/ssl/PRODPUBLIC.pem'
+WEBHOOK = 'https://prod.expensebot.design/'
 
-with open('/var/www/expenseBot/bot.token','r') as fichier:
-    token = fichier.read()
-    token = token.replace('\n','')
-
-bot = Bot(token)
